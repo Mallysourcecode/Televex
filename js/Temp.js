70e13ceaed5e52092cc42a6114e0fe12305cefea
Nnebedum Malachy Elochukwu(Mally) <107500910+Mallysourcecode@users.noreply.github.com>
2022-06-15 18:10:05 +0100

    First Commit
    First Date: 14-05-2022

---
+++ js/Temp.js
@@ -0,0 +1,66 @@
+// JQUERY TO HIDE
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   $("#tst").toggle(500);
+// });
+
+// JQUERY TO SHOW
+// $("#btn-show").click(function (e) {
+//   e.preventDefault();
+//   $("#tst").show(3000);
+// });
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   $("#tst").slideToggle(200);
+// });
+
+//ANIMATE
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   $("#tst").animate(
+//     {
+//       marginleft: "30px",
+//       color: "red",
+//       fontSize: "40px",
+//       background: "orange",
+//       width: "720px",
+//     },
+//     3000
+//   );
+// });
+
+//CALLBACK
+
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   $("#txt").hide(5000, function () {
+//     alert("element hiden");
+//   });
+// });
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   var txt = $("#txt").text(
+//     "ojpriuithrejwiwkopqlwokiejjdf dsjakmPLPOKQAISDJ VSAWIEJFV "
+//   );
+//   console.log(txt);
+// });
+
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   console.log(
+//     $("#txt").html(
+//       "<h1> Welcome to h1 element</h1><a href ='#'>the new link</a>"
+//     )
+//   );
+// });
+
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   console.log($("#ipt").val("new jqvalue"));
+// });
+
+// $("#btn").click(function (e) {
+//   e.preventDefault();
+//   var val = $("#ipt").attr("type", "text");
+//   console.log(val);
+// });
